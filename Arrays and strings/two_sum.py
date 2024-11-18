a64cb3ff3ef4a89db4dae7b4503b63f2c69cb477
Seulgie Han <gkstmfrl917@naver.com>
2024-11-18 09:05:15 +0100

    Create two_sum.py

---
+++ Arrays and strings/two_sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            curr = nums[left] + num[right]
+            if curr = target:
+                return
